6c7ad565af8d8df2c2b29ed73d4c714ee75335e3
Jakub Woźny <jakubovip@gmail.com>
2020-04-27 15:38:34 +0200

    Add alert

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,20 @@ import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from 'app/app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterModule, AppRoutingModule, HttpClientModule, NgbModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule,
+    ToastrModule.forRoot(),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/detected-devices/modal-time-picker/modal-time-picker.component.ts
+++ src/app/detected-devices/modal-time-picker/modal-time-picker.component.ts
@@ -1,14 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { FormControl } from '@angular/forms';
 import { HttpWrapperService } from 'core/services/http-wrapper.service';
+import { ToastrService } from 'ngx-toastr';
+import { AutoUnsubscribe } from 'ngx-auto-unsubscribe';
+import { formatDate } from '@angular/common';
 
+@AutoUnsubscribe()
 @Component({
   selector: 'app-modal-time-picker',
   templateUrl: './modal-time-picker.component.html',
   styleUrls: ['./modal-time-picker.component.scss'],
 })
-export class ModalTimePickerComponent {
+export class ModalTimePickerComponent implements OnDestroy {
   @Input() public username: string;
   @Input() public pushToken: string;
 
@@ -19,8 +23,12 @@ export class ModalTimePickerComponent {
 
   constructor(
     public activeModal: NgbActiveModal,
-    private readonly httpWrapper: HttpWrapperService
+    private readonly httpWrapper: HttpWrapperService,
+    private readonly toastr: ToastrService
   ) {}
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  public ngOnDestroy(): void {}
 
   public changeNotificationTime(): void {
     const notificationTime = new Date();
@@ -30,8 +38,18 @@ export class ModalTimePickerComponent {
       0
     );
 
-    this.httpWrapper.changeNotificationTime(this.pushToken, {
-      time: notificationTime,
-    });
+    this.httpWrapper
+      .changeNotificationTime(this.pushToken, {
+        time: notificationTime,
+      })
+      .subscribe(() =>
+        this.toastr.success(
+          `Notification time for ${this.username} was changed to ${formatDate(
+            notificationTime,
+            'shortTime',
+            'en-US'
+          )}`
+        )
+      );
   }
 }
